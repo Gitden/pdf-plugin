113986b28e5f4b5f6654f387b6486bfc27796ae6
Gitden-hyjeon <hyjeon@gitden.com>
2016-01-05 19:50:07 +0900

    불필요한 파일 제거.
    불필요한 파일 제거.

--- gd-pdfview-1.0-source/platform/android/src/com/gitden/pdf/LinkInfoVisitor.java
+++
@@ -1,7 +0,0 @@
-package com.gitden.pdf;
-
-abstract public class LinkInfoVisitor {
-	public abstract void visitInternal(LinkInfoInternal li);
-	public abstract void visitExternal(LinkInfoExternal li);
-	public abstract void visitRemote(LinkInfoRemote li);
-}
